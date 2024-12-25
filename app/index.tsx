b96b2bd179d5dbb15d9c3497245ea7fdea9d2a9b
Lucienzan <changwilliam826@gmail.com>
2024-12-25 12:52:38 +0630

    [main] #default folder

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const app = () => {
+  return (
+    <View style={{ backgroundColor: 'red' }}>
+      <Text>app</Text>
+    </View>
+  )
+}
+
+export default app
